4f50c76043724442b5de4edacf07c4d5a088f0d5
peter-lyr <llydrp.ldp@gmail.com>
2024-04-30 00:41:47 +0800

    startuptime

--- plugin/.dp_tabline.lua
+++ plugin/.dp_tabline.lua
@@ -1,6 +1,8 @@
+local starttime = vim.fn.reltime()
 require 'dp_tabline'
-local t = vim.fn.reltimefloat(vim.fn.reltime(StartTime))
+local t1 = vim.fn.reltimefloat(vim.fn.reltime(starttime))
+local t2 = vim.fn.reltimefloat(vim.fn.reltime(StartTime))
 if not StartTimeList then
   StartTimeList = {}
 end
-StartTimeList[#StartTimeList+1] = string.format("`%.3f` [%s]", t, vim.fn.fnamemodify(debug.getinfo(1)['source'], ':t:r'))
+StartTimeList[#StartTimeList+1] = string.format("`%.3f` `%.3f` [%s]", t2, t1, vim.fn.fnamemodify(debug.getinfo(1)['source'], ':t:r'))
