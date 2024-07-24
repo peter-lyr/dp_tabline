83236e532c7e8a882a5479b0a53281d56b672911
peter-lyr <llydrp.ldp@gmail.com>
2024-07-24 10:12:46 +0800

    fix: SLPH__1()

--- lua/dp_tabline.lua
+++ lua/dp_tabline.lua
@@ -106,8 +106,10 @@ function SLPH__1()
     if B.is(projroot__1) then
       M.projhead__1 = B.rep_slash(B.file_parent(projroot__1)) .. '/'
       M.projtail__1 = B.get_only_name(projroot__1) .. '/'
+      M.projhead = string.sub(projroot, #projroot__1 + 2, #projroot) .. '/'
+    else
+      M.projhead = B.rep_slash(B.file_parent(projroot)) .. '/'
     end
-    M.projhead = B.rep_slash(B.file_parent(projroot)) .. '/'
     M.projtail = B.get_only_name(projroot) .. '/'
     M.fnamehead = vim.fn.fnamemodify(fname, ':h') .. '/'
     if M.fnamehead == './' then
