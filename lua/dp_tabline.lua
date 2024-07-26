a2b6f40d9f723e10f0b0959b125d295436e2c097
peter-lyr <llydrp.ldp@gmail.com>
2024-07-26 10:07:26 +0800

    python lua\dp_tabline.luas lua\dp_tabline.luas lua\dp_tabline.luas

--- lua/dp_tabline.lua
+++ lua/dp_tabline.lua
@@ -63,7 +63,7 @@ M.bufs_root              = 'git'
 M.nvim_exe_cpu_usage_py  = B.getcreate_file(B.get_source_dot_dir(M.source), 'nvim_exe_cpu_usage.py')
 M.nvim_exe_cpu_usage_txt = B.getcreate_file(B.get_source_dot_dir(M.source), 'nvim_exe_cpu_usage.txt')
 
-B.system_run('start silent', '%s %s %s', M.nvim_exe_cpu_usage_py, vim.fn.getpid(), M.nvim_exe_cpu_usage_txt)
+B.system_run('start silent', 'python %s %s %s', M.nvim_exe_cpu_usage_py, vim.fn.getpid(), M.nvim_exe_cpu_usage_txt)
 
 function M.get_head_root_tail(file)
   local head_root = B.rep_slash(B.get_proj_root(file))
